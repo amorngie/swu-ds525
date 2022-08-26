f902efdb7545baff0d9dab7824fc29616cd2bf3c
amorngie <j.amornkarn@gmail.com>
2022-08-26 14:04:51 +0000

    Create all tables

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -1,12 +1,44 @@
 import psycopg2
 
 
-table_drop = "DROP TABLE IF EXISTS songplays"
+table_drop = "DROP TABLE IF EXISTS actor;DROP TABLE IF EXISTS org;DROP TABLE IF EXISTS repo;DROP TABLE IF EXISTS events"
 
 table_create = """
-    CREATE TABLE IF NOT EXISTS xxx (
-    )
+CREATE TABLE IF NOT EXISTS actor (
+                actor_id INTEGER PRIMARY KEY,
+                login VARCHAR(50) NOT NULL,
+                display_login VARCHAR(100) NOT NULL,
+                gravatar_id VARCHAR(100),
+                url VARCHAR(255) NOT NULL,
+                avatar_url VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS org (
+                org_id INTEGER PRIMARY KEY,
+                login VARCHAR(50) NOT NULL,
+                gravatar_id VARCHAR(100),
+                url VARCHAR(255) NOT NULL,
+                avatar_url VARCHAR(255) NOT NULL
+
+);
+CREATE TABLE IF NOT EXISTS repo (
+                repo_id INTEGER PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                url VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS events (
+                events_id INTEGER PRIMARY KEY,
+                actor_id INTEGER NOT NULL,
+                org_id INTEGER NOT NULL,
+                repo_id INTEGER NOT NULL,
+                type VARCHAR NOT NULL,
+                public VARCHAR NOT NULL,
+                created_at TIMESTAMP NOT NULL,
+                CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actor(actor_id) ,
+                CONSTRAINT fk_org FOREIGN KEY(org_id) REFERENCES org(org_id) ,
+                CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repo(repo_id)
+)
 """
+
 
 create_table_queries = [
     table_create,
@@ -16,7 +48,7 @@ drop_table_queries = [
 ]
 
 
-def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+def drop_tables(cur, conn) -> None:
     """
     Drops each table using the queries in `drop_table_queries` list.
     """
@@ -25,7 +57,7 @@ def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
         conn.commit()
 
 
-def create_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
+def create_tables(cur, conn) -> None:
     """
     Creates each table using the queries in `create_table_queries` list.
     """
